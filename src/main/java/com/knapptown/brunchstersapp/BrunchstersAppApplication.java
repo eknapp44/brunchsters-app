b9baa38692aaf7c4589bab5743eb6f6e88ad4496
Evan <pnkme45@gmail.com>
2022-02-25 23:57:02 -0600

    Initial Commit
    Base Gradle repository.

---
+++ src/main/java/com/knapptown/brunchstersapp/BrunchstersAppApplication.java
@@ -0,0 +1,13 @@
+package com.knapptown.brunchstersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunchstersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunchstersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knapptown/brunchstersapp/BrunchstersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knapptown.brunchstersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunchstersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
